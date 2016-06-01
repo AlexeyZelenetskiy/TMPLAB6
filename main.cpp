b3032d2c3392fe9d996a78d6fee83037ddbbbe74
AlexeyZelenetskiy <Zelenetskiy_alexey@mail.ru>
2016-06-02 00:12:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  timp-6
+//
+//  Created by Павел Наумов on 16.05.16.
+//  Copyright © 2016 Ayo Techs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
